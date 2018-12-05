c6cc9f552d945e2fa9cedd5fa1425e9a9fe9dacf
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-12-04 21:57:51 -0800

    added validation into inquirer for product selection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,19 +11,50 @@ const keys = dotenv.config();
 const connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: keys.parsed.SQLPASS,
   database: "bamazon"
 });
 
-connection.query("SELECT * FROM products", function(err, res) {
-  if (err) throw err;
-  console.table(res);
-  connection.end();
-});
+function dbCon(){
+  console.log("WELCOME TO BAMZAON \n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    store();
+  });
+}
+
+function store(){
+  inquirer.prompt([
+    {
+    type: "input",
+    default: "number",
+    message: "Enter the product ID you'd like to purchase",
+    name: "item",
+    validate: validate
+  },
+  {
+    type: "input",
+    default: "number",
+    message: "How many would you like?",
+    name: "quantity",
+    validate: validate
+  }
+  ])
+  .then(function(answer){
+      console.log(answer.item, answer.quantity);
+      connection.end();
+  })
+}
+
+function validate(name)
+{
+   let reg = /^\d+$/;
+   return reg.test(name) || "Input should be a number!";
+}
+
+dbCon();
