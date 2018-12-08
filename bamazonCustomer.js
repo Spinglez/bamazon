d7649568c564360f40b90c009af74d754e060c0e
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-12-07 21:35:43 -0800

    should have committed more often on this one, added database schema so it can be more easily cloned, added purchase function that displays what is purchased, also added validation on amount if it's more than whats in stock will return to store if item amounts are zero it will return to store, added a continue shopping function as well. Everything working at this time. TODO: add validation on selected item. If the item selected is an integer it will allow you to continue, need to have it check if an inproper item is selected.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,10 +46,56 @@ function store(){
   }
   ])
   .then(function(answer){
+    let query = "SELECT * FROM products WHERE ?"
       console.log(answer.item, answer.quantity);
-      connection.end();
+      connection.query(query, {item_id: answer.item}, function(err,res){
+        if (err) throw err;
+        else if (res[0].stock_quantity <= 0 || res[0].stock_quantity - answer.quantity <= 0) {
+          console.log("\nthere's not sufficient quantity to purchase, please select an available amount or another item while we re-stock");
+          store();
+        }
+        else {
+          updateStock(res[0].stock_quantity, answer.quantity, answer.item);
+          // connection.query(stock, [{stock_quantity: res[0].stock_quantity - answer.quantity}, {item_id: answer.item}],function(err,resp))
+          console.log("you're purchasing:",answer.quantity,res[0].product_name +"(s)","your total is: $"+answer.quantity * res[0].price);
+        }
+      })
+      // connection.end();
   })
 }
+
+function contShop(){
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Continue Shopping?",
+      choices: ['Yes','No'],
+      name: 'continue'
+    }
+  ]).then(function(answer){
+    switch (answer.continue) {
+      case 'Yes':
+        dbCon();
+        break;
+      case 'No':
+        console.log("Thanks for shopping with us!");
+        connection.end();
+        break;
+      default:
+        dbCon();
+    }
+  })
+}
+
+// for this function x = current stock, y = amount to purchase, and z = the item_id of what they selected
+function updateStock(x, y, z){
+  let stock = "UPDATE products SET ? WHERE ?"
+  connection.query(stock, [{stock_quantity: x - y}, {item_id: z}],function(err,resp){
+    if (err) throw err;
+    console.log(x - y,"Inventory updated!");
+    contShop();
+  })
+};
 
 function validate(name)
 {
